c95cdfed674ffa6ad67a7a57159a57ac7406cb30
Ben Yellin <byellindc@gmail.com>
2019-04-03 16:37:03 -0400

    setup css

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import store from "./redux/store";
 import { Provider } from "react-redux";
 
-import './index.css';
+import './index.scss';
 import App from './App';
 
 // import serviceWorker from "./serviceWorker";
