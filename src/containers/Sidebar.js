9c0eaf84ad7ca32aba370e4f35604ad9eee42199
Ben Yellin <byellindc@gmail.com>
2019-04-04 15:32:16 -0400

    cleanup sidebar

--- src/containers/Sidebar.js
+++ src/containers/Sidebar.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-import SidebarItem from '../components/SidebarItem'
+import SidebarItem from '../components/SidebarItem';
 
 class Sidebar extends Component {
   render() {
@@ -16,7 +16,7 @@ class Sidebar extends Component {
 const mapStateToProps = state => {
   return {
     documents: state.documents
-  }
+  };
 }
 
 export default connect(mapStateToProps)(Sidebar);
