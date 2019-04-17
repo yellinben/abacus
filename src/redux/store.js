f4a0c2b236542f61c172f1cfa7cc67344eef999e
Ben Yellin <byellindc@gmail.com>
2019-04-17 14:24:27 -0400

    enable devtools tracing

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,9 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducer';
 import thunk from 'redux-thunk';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ trace: true, traceLimit: 25 }) || compose;
 
 const store = createStore(
   rootReducer, 
