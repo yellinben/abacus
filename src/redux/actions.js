fae7cdb69ade2efe34b1b6d4d5075bc95a5fc56c
Ben Yellin <yellinben@gmail.com>
2019-05-30 16:04:24 -0400

    Format redux actions

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,8 +1,6 @@
-import { isEmpty } from 'lodash'
-
-import { 
-  FETCHED_DOCUMENTS, 
-  FETCHED_DOCUMENT, 
+import {
+  FETCHED_DOCUMENTS,
+  FETCHED_DOCUMENT,
   DOCUMENT_CREATED,
   DOCUMENT_DELETED,
   DOCUMENT_UPDATED,
@@ -19,42 +17,43 @@ import {
   UPDATE_EDITOR_STATE,
   CATCH_ERROR,
   WRITE_EDITOR,
-  WRITE_RESULTS
-} from './types'
+  WRITE_RESULTS,
+} from './types';
 
 export const apiURL = (...paths) => {
   const API = {
-    host: "http://localhost",
+    host: 'http://localhost',
     port: 3000,
-    path: "api",
-    version: 1
-  }
+    path: 'api',
+    version: 1,
+  };
 
-  const url = [`${API.host}:${API.port}`, API.path, `v${API.version}`]
+  const url = [`${API.host}:${API.port}`, API.path, `v${API.version}`];
   if (paths && paths.length) url.push(...paths);
-  
+
   return url.join('/');
-}
+};
 
 export const creatingDocument = () => {
   return dispatch => {
     // console.log('creatingDocument');
-    fetch(apiURL('documents'), {method: "POST"})
+    fetch(apiURL('documents'), {method: 'POST'})
       .then(res => res.json())
       .then(doc => {
-        const href = (doc.id) ? `/documents/${doc.id}` : '/';
+        const href = doc.id ? `/documents/${doc.id}` : '/';
         window.location.href = href;
         // return dispatch(createdDocument(doc));
-      }).catch(err => {
+      })
+      .catch(err => {
         console.error(err);
         return dispatch(catchError(err));
       });
-  }
-}
+  };
+};
 
-export const createdDocument = (doc) => {
+export const createdDocument = doc => {
   return {action: DOCUMENT_CREATED, payload: doc};
-}
+};
 
 export const fetchingDocuments = () => {
   return dispatch => {
@@ -64,13 +63,13 @@ export const fetchingDocuments = () => {
         dispatch(fetchedDocuments(docs));
       });
   };
-}
+};
 
-export const fetchedDocuments = (documents) => {
+export const fetchedDocuments = documents => {
   return {type: FETCHED_DOCUMENTS, payload: documents};
-}
+};
 
-export const fetchingDocument = (id) => {
+export const fetchingDocument = id => {
   return dispatch => {
     fetch(apiURL('documents', id))
       .then(res => res.json())
@@ -78,41 +77,41 @@ export const fetchingDocument = (id) => {
         dispatch(fetchedDocument(doc));
       });
   };
-}
+};
 
-export const fetchedDocument = (document) => {
+export const fetchedDocument = document => {
   return {type: FETCHED_DOCUMENT, payload: document};
-}
+};
 
-export const updatingDocument = (doc) => {
+export const updatingDocument = doc => {
   return dispatch => {
     dispatch(updatedDocument(doc));
     fetch(apiURL('documents', doc.id), {
-      method: "PATCH",
-      headers: {"Content-Type": "application/json"},
-      body: JSON.stringify({title: doc.title})
+      method: 'PATCH',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({title: doc.title}),
     });
-  }
-}
+  };
+};
 
-export const updatedDocument = (doc) => {
+export const updatedDocument = doc => {
   return {type: DOCUMENT_UPDATED, payload: doc};
-}
+};
 
-export const deletingDocument = (doc) => {
+export const deletingDocument = doc => {
   return dispatch => {
     dispatch(deletedDocument(doc));
     fetch(apiURL('documents', doc.id), {
-      method: "DELETE",
-      headers: {"Content-Type": "application/json"},
-      body: JSON.stringify(doc)
+      method: 'DELETE',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(doc),
     });
-  }
-}
+  };
+};
 
-export const deletedDocument = (doc) => {
+export const deletedDocument = doc => {
   return {type: DOCUMENT_DELETED, payload: doc};
-}
+};
 
 // export const updateResults = (editorState) => {
 //   return {type: UPDATE_RESULTS, payload: editorState};
@@ -121,21 +120,22 @@ export const deletedDocument = (doc) => {
 export const updatingDocumentContent = (doc, contents) => {
   return dispatch => {
     fetch(apiURL('documents', doc.id), {
-      method: "PATCH",
-      headers: {"Content-Type": "application/json"},
-      body: JSON.stringify({contents})
-    }).then(res => res.json())
-      .then(doc => dispatch(updatedDocument(doc)))
-  }
-}
+      method: 'PATCH',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({contents}),
+    })
+      .then(res => res.json())
+      .then(doc => dispatch(updatedDocument(doc)));
+  };
+};
 
-export const writeEditorText = (text) => {
-  return {type: WRITE_EDITOR, payload: text}
-}
+export const writeEditorText = text => {
+  return {type: WRITE_EDITOR, payload: text};
+};
 
-export const writeResultText = (text) => {
-  return {type: WRITE_RESULTS, payload: text}
-}
+export const writeResultText = text => {
+  return {type: WRITE_RESULTS, payload: text};
+};
 
 export const updatingEditor = (doc, rawContent) => {
   return dispatch => {
@@ -143,36 +143,38 @@ export const updatingEditor = (doc, rawContent) => {
     const contents = rawContent.blocks.map(b => b.text);
 
     fetch(apiURL('documents', doc.id), {
-      method: "PATCH",
-      headers: {"Content-Type": "application/json"},
-      body: JSON.stringify({contents})
-    }).then(res => res.json())
+      method: 'PATCH',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({contents}),
+    })
+      .then(res => res.json())
       .then(newDoc => {
         const results = newDoc.lines.map(l => l.result_formatted);
         dispatch(updatedResults(results));
-      }).catch(err => {
+      })
+      .catch(err => {
         console.error(err);
         return dispatch(catchError(err));
       });
-  }
-}
+  };
+};
 
-export const updatedEditorState = (editorState) => {
+export const updatedEditorState = editorState => {
   return {type: UPDATE_EDITOR_STATE, payload: editorState};
-}
+};
 
-export const updatedEditorContent = (content) => {
+export const updatedEditorContent = content => {
   return {type: UPDATE_EDITOR, payload: content};
-}
+};
 
-export const updatedResults = (results) => {
+export const updatedResults = results => {
   return {type: UPDATE_RESULTS, payload: results};
-}
+};
 
 export const toggleDebug = () => {
   return {type: TOGGLE_DEBUG};
-}
+};
 
-export const catchError = (err) => {
+export const catchError = err => {
   return {type: CATCH_ERROR, payload: err};
-}
+};
